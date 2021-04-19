4cfddef393231928e995c65c0601a1f78c692dc3
simongraf2 <82580323+simongraf2@users.noreply.github.com>
2021-04-19 15:58:17 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-def on_received_number(receivedNumber):
-    pause(200)
-    signalStrength = radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH)
-    basic.show_number(signalStrength)
-radio.on_received_number(on_received_number)
-
 signalStrength = 0
 radio.set_group(1)
+thisTime = control.millis()
+
+def on_received_number(receivedNumber):
+    global signalStrength
+    signalStrength = radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH)
+radio.on_received_number(on_received_number)
 
 def on_forever():
+    global signalStrength
     radio.send_number(3)
     basic.show_number(signalStrength)
+    if control.millis() - thisTime > 1000:
+        if lastSignalStrength > signalStrength:
+            # wrong way --> turn
+            basic.show_string("w")
+            pass
+        else:
+            # right way --> keep Direction
+            basic.show_string("r")
+            pass
+        thisTime = control.millis() 
+        lastSignalStrength = signalStrength
+    pause(100)
+
 basic.forever(on_forever)
+
--- main.ts
+++ main.ts
@@ -1,11 +1,30 @@
-radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
-    pause(200)
-    let signalStrength = radio.receivedPacket(RadioPacketProperty.SignalStrength)
-    basic.showNumber(signalStrength)
-})
 let signalStrength = 0
 radio.setGroup(1)
+let thisTime = control.millis()
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    
+    signalStrength = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+})
 basic.forever(function on_forever() {
+    let thisTime: number;
+    let lastSignalStrength: number;
+    
     radio.sendNumber(3)
     basic.showNumber(signalStrength)
+    if (control.millis() - thisTime > 1000) {
+        if (lastSignalStrength > signalStrength) {
+            //  wrong way --> turn
+            basic.showString("w")
+            
+        } else {
+            //  right way --> keep Direction
+            basic.showString("r")
+            
+        }
+        
+        thisTime = control.millis()
+        lastSignalStrength = signalStrength
+    }
+    
+    pause(100)
 })
